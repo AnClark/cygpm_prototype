c91b0bd94084cba01c28500bbeee6bbc155c6823
AnClark Liu <clarklaw4701@qq.com>
2019-11-26 11:57:27 +0800

    database: main: Use db.getErrorLevel() to get db open state

--- database/main.cpp
+++ database/main.cpp
@@ -5,6 +5,14 @@ const char* DATABASE_NAME = "./cygpm.db";
 int main()
 {
     CygpmDatabase db(DATABASE_NAME);
+    if(db.getErrorLevel() != 0)
+    {
+        cerr << "ERROR: Failed to open database. Abort." << endl;
+        return -1;
+    }
+
     db.createTable();
     db.parseAndBuildDatabase("../test/setup.ini");
+
+    return 0;
 }
