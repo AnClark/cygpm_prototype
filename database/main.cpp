5322a3a6740c38f643243508b46c92823ee1f5c4
AnClark Liu <clarklaw4701@qq.com>
2019-12-07 17:29:04 +0800

    database: main: Remove database/journal before building database
    Database should always be regenerated when update, so removing it at
    first instead of dropping tables will save time making a large journal
    as backup.
    
    Also, to collaborate with DB Browser, if a database is occupied (usually
    by DB Browser), database will not be removed, then fall back to "DROP
    TABLE IF EXISTS".

--- database/main.cpp
+++ database/main.cpp
@@ -1,9 +1,16 @@
 #include "database.h"
+#include <cerrno>
+#include <fstream>
 
 const char *DATABASE_NAME = "./cygpm.db";
+const char *DATABASE_JOURNAL = "./cygpm.db-journal";
+
+void removeOldDatabase();
 
 int main()
 {
+    removeOldDatabase();
+
     CygpmDatabase db(DATABASE_NAME);
     if (db.getErrorLevel() != 0)
     {
@@ -16,4 +23,33 @@ int main()
     db.buildDependencyMap();
 
     return 0;
+}
+
+void removeOldDatabase()
+{
+    cerr << "Removing old database and journal" << endl;
+
+    ifstream ftest_dbname(DATABASE_NAME), ftest_dbjournal(DATABASE_JOURNAL);
+
+    if (ftest_dbname) // Remove database file
+    {
+        ftest_dbname.close();
+
+        if (remove(DATABASE_NAME) == 0)
+            cerr << "Removed old database" << endl;
+        else
+        {
+            cerr << "ERROR: Failed to remove database. Will use current database" << endl;
+            return; // Keep journal if database still exist
+        }
+    }
+    if (ftest_dbjournal) // Remove SQLite database journal
+    {
+        ftest_dbjournal.close();
+
+        if (remove(DATABASE_JOURNAL) == 0)
+            cerr << "Removed old journal" << endl;
+        else
+            cerr << "ERROR: Failed to remove journal" << endl;
+    }
 }
