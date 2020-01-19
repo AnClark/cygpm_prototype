2c6cf4167ed6227685717e8bf7b093341a3451c7
AnClark Liu <clarklaw4701@qq.com>
2020-01-19 10:17:05 +0800

    database: main: Add new tests
    - findDependencies() test
    - Package info getting

--- database/main.cpp
+++ database/main.cpp
@@ -5,11 +5,12 @@
 const char *DATABASE_NAME = "./cygpm.db";
 const char *DATABASE_JOURNAL = "./cygpm.db-journal";
 
-void removeOldDatabase();
+/** Internal functions **/
+inline void removeOldDatabase();
 
 int main()
 {
-    removeOldDatabase();
+    //removeOldDatabase();
 
     CygpmDatabase db(DATABASE_NAME);
     if (db.getErrorLevel() != 0)
@@ -18,16 +19,47 @@ int main()
         return -1;
     }
 
+#if 0
     db.createTable();
     db.parseAndBuildDatabase("../test/setup.ini");
     db.buildDependencyMap();
 
     cout << "Added " << db.getNumPackages() << " packages" << endl;
+#endif
+
+#if 0
+    cout << db.getNewestVersion("bash") << endl;
+    
+    vector<string> dl;
+    db.findDependencies(dl, "bash", NULL);
+
+    for(auto i = dl.begin(); i != dl.end(); i++)
+        cout << *i << ' ';
+    cout << endl;
+#endif
+#define TEST_PACKAGE "python3"
+
+    cout << "SDESC: " << db.getShortDesc(TEST_PACKAGE) << endl;
+    cout << "LDESC: " << db.getLongDesc(TEST_PACKAGE) << endl;
+    cout << "Category: " << db.getCategory(TEST_PACKAGE) << endl;
+
+    cout << "Install pak path: " << db.getInstallPakPath(TEST_PACKAGE, NULL) << endl;
+    cout << "Install pak size: " << db.getInstallPakSize(TEST_PACKAGE, NULL) << endl;
+    cout << "Install pak SHA512: " << db.getInstallPakSHA512(TEST_PACKAGE, NULL) << endl;
+    cout << "Source pak path: " << db.getSourcePakPath(TEST_PACKAGE, NULL) << endl;
+    cout << "Source pak size: " << db.getSourcePakSize(TEST_PACKAGE, NULL) << endl;
+    cout << "Source pak SHA512: " << db.getSourcePakSHA512(TEST_PACKAGE, NULL) << endl;
+
+    cout << "Available versions: " << db.getNewestVersion(TEST_PACKAGE) << ' ';
+    vector<const char *> pvs = db.getPrevVersions(TEST_PACKAGE);
+    for (auto i = pvs.begin(); i != pvs.end(); i++)
+        cout << *i << ' ';
+    cout << endl;
 
     return 0;
 }
 
-void removeOldDatabase()
+inline void removeOldDatabase()
 {
     cerr << "Removing old database and journal" << endl;
 
